22de73ac71f7ca4461d9f48c28ab2efba40835c7
Md Kamran Hossain <132159379+mdkamranhossain@users.noreply.github.com>
2025-02-07 23:40:09 +0600

    more

---
+++ combing.js
@@ -0,0 +1,13 @@
+/**
+ * 5. Combining Arrays
+Instructions:
+
+Create two arrays of your choice.
+Use the concat method to combine the two arrays into a new array.
+Print both the original arrays and the combined array using console.log().
+ */
+
+let fruit = ['apple', 'mango', 'liche']
+let vegetable = ['carrot', 'cucumber']
+let conbainArray = fruit.concat(vegetable);
+console.log(conbainArray)
